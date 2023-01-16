b1c65f103e7a6b45c3479db0d116cd5e50307591
PanJohnny <devstefancz@gmail.com>
2023-01-16 20:45:10 +0100

    :eyes: pow 2

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-import * as mongoose from "mongoose";
+import mongoose from "mongoose";
 // { Schema, model, connect, connection, models }
 if (mongoose.connection.readyState != 1) {
     const login =
