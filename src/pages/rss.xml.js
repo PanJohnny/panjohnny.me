b576f608663510890e15d441a65ef3fb466fdc39
PanJohnny <janstefanca@seznam.cz>
2024-04-11 19:08:54 +0200

    Bumped node, removed unused code and fixed latest blogs and sorting

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,18 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import {getCollection} from 'astro:content';
 
-export async function get(context) {
-  const blog = await getCollection('blog');
-  return rss({
-    title: 'PanJohnny’s Blog',
-    description: 'Posts written by a guy doing coding for fun',
-    site: "https://panjohnny.vercel.app",
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      customData: "<tags>" + post.data.tags + "</tags>",
-      link: `/blog/${post.slug}/`,
-    })),
-  });
+export async function GET(context) {
+    const blog = await getCollection('blog');
+    return new Response(await rss({
+        title: 'PanJohnny’s Blog',
+        description: 'Posts written by a guy doing coding for fun',
+        site: "https://panjohnny.me",
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            customData: "<tags>" + post.data.tags + "</tags>",
+            link: `/blog/${post.slug}/`,
+        })),
+    }).then(res => res.blob()), {status: 200});
 }
