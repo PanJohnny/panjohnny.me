f0a90273d93bbe76a96905e040bb2baa783526d2
Jan Štefanča <janstefanca@seznam.cz>
2025-09-05 21:28:31 +0200

    add fix for indexed sites

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://panjohnny.me',
-  integrations: [sitemap(), icon()],
+  integrations: [sitemap({
+    filter: (page) => /\/projects\/[^\/]+/.test(page) && !page.includes('/tag/')
+  }), icon()],
   i18n: {
     locales: ['en', 'cs'],
     defaultLocale: 'en',
